c8852cdd10dd2a6c015463bc6efc5dc40ae51cd8
Israel Salinas Martínez <israel.salinas.m@gmail.com>
2020-08-01 13:23:18 -0500

    Updates

---
+++ precache-manifest.2bf1c4d74f0f0e6dd5e2b1fd7f9356cb.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6b81c546f102a12250f5791e583bf2c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "afabef5a644306099c6b",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ddf4117ecd67be0c6df2",
+    "url": "/portfolio/static/css/main.cea229fb.chunk.css"
+  },
+  {
+    "revision": "afabef5a644306099c6b",
+    "url": "/portfolio/static/js/2.f81611e2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.f81611e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddf4117ecd67be0c6df2",
+    "url": "/portfolio/static/js/main.0fd04179.chunk.js"
+  },
+  {
+    "revision": "c8cb60120edc498f008e",
+    "url": "/portfolio/static/js/runtime-main.f333858c.js"
+  },
+  {
+    "revision": "8e43a7a4b66e0877ab06fb9b1fc3be42",
+    "url": "/portfolio/static/media/bg-1.8e43a7a4.png"
+  },
+  {
+    "revision": "23be9423712efc058eff6cdf71312dde",
+    "url": "/portfolio/static/media/carousel-1.23be9423.png"
+  },
+  {
+    "revision": "1a44c97514c115c5ea36df173a9f2184",
+    "url": "/portfolio/static/media/carousel-2.1a44c975.jpg"
+  },
+  {
+    "revision": "0686d44d311f49e900b6a95ab1b4e638",
+    "url": "/portfolio/static/media/carousel-3.0686d44d.png"
+  },
+  {
+    "revision": "89b0fc52130c18f26763b847c39c5faf",
+    "url": "/portfolio/static/media/logo-kodemia.89b0fc52.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9583c1e083dc58b7e16ea09616e9d89e",
+    "url": "/portfolio/static/media/preview-messebau-virtual.9583c1e0.png"
+  },
+  {
+    "revision": "4b55689df38f759a53cfd4f28a74f1fa",
+    "url": "/portfolio/static/media/preview-messebau.4b55689d.png"
+  },
+  {
+    "revision": "26bd0e6a2c9d196b4e172e08cba74ead",
+    "url": "/portfolio/static/media/preview-skillup.26bd0e6a.png"
+  }
+]);
